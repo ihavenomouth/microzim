778ae42d56c797740294cf8254a50e6446d328f9
Javier <javier@a.com>
2021-06-12 20:59:58 +0200

    negritas, cursivas, readme, cabecera...

--- README.md
+++ README.md
@@ -1,18 +1,104 @@
 # MicroZim
 
-A very simple Zim-Wiki plugin for Micro-editor.
+Un plugin de ZIM-Wiki muy sencillo para Micro-editor.  
 
-## Installation
+A very simple ZIM-Wiki plugin for Micro-editor.  
 
-Copy the files file into ~/.config/micro/plug/microzim
 
-# Default binding keys
+Micro github: [https://github.com/zyedidia/micro]
+ZIM website: [https://zim-wiki.org/index.html]  
+ZIM github: [https://github.com/zim-desktop-wiki/zim-desktop-wiki]  
+
+
+## ¿Por qué?
+
+Uso ZIM a diario y micro editor es mi editor por defecto en la terminal. ZIM tiene los mismos atajos de teclado para los encabezados que LibreOffice Writer, otro fantástico programa de software libre con el que estoy familiarizado. En resumen, era una pena que no tuviera una manera cómoda de editar ficheros creados con ZIM (o crearlos directamente) desde la terminal usando micro.
+
+## Why?
+
+I've been using ZIM on a daily basis for years and micro is my choice to edit texts in the terminal. ZIM has the same keyboard shortcuts for the headings as LibreOffice Writer, another superb free software I'm very used to. It was a pity not to have a comfortable way to edit ZIM files in micro.
+
+
+# Instalación / Installation
+
+Copia los ficheros a ~/.config/micro/plug/microzim (tendrás que crear ese directorio)  
+
+Copy the files file into ~/.config/micro/plug/microzim (you'll ned to create the directory)
+
+
+# Teclas por defecto / Default binding keys
+
+## Español
+
+* Alt+1 es encabezado de nivel 1
+* Alt+2 es encabezado de nivel 2
+* Alt+3 es encabezado de nivel 3
+* Alt+4 es encabezado de nivel 4
+* Alt+5 es encabezado de nivel 5
+* Alt+t hará que el texto seleccionada sea verbatim (tal cual, al pie de la letra, literalmente)
+* Alt+h añadirá la cabecera de Zim-wiki al fichero
+* Alt+b hará que las palabras selecionadas se conviertan en negritas
+* Alt+i hará que las palabras selecionadas se conviertan en cursivas
+
+Algunos de estos atajos de teclado pueden estar ya en uso en tu terminal (o en tu sistema). Cámbialas si lo necesitas.  
+
+Por otro lado, siempre puedes usar las siguientes órdenes en micro (pulsa Ctrl+E y escribe la orden):  
+* h1
+* h2
+* h3
+* h4
+* h5
+* verbatim
+* header
+* bold
+* italic
+
+
+## English
 
 * Alt+1 is heading 1
 * Alt+2 is heading 2
 * Alt+3 is heading 3
 * Alt+4 is heading 4
 * Alt+5 is heading 5
-* Alt+t will render the selected paragraph as verbatim
+* Alt+t will render the selected paragraphs (or words) as verbatim
+* Alt+h will create the Zim-wiki header
+* Alt+b will render the selected words as bold
+* Alt+i will render the selected words as italic
+
+Some of these shortcuts can be already in use by your terminal emulator. You can change them if you need to.  
+
+On the other hand, you can use the following commands inside micro:  
+* h1
+* h2
+* h3
+* h4
+* h5
+* verbatim
+* header
+* bold
+* italic
 
 
+# Cositas 
+
+* Tanto las negritas como las cursivas sólo pueden ser utilizadas sobre palabras de la misma línea (ZIM permite una mayor flexiblidad, pero requiere más tiempo del que me parece razonable dedicarle a esto). El funcionamiento actual es más que suficiente para mí, pero oye, mejóralo tú si quieres.  
+* Tanto los comentarios en el código como en la barra de estado de ZIM están en español, siénte libre de adaptarlo a tus necesidades lingüísticas.  
+* Esto es sólo un proyecto personal y tal cual está es útil para mí y satisface mis necesidades (que evidentemente son las más importantes para mí), de modo que es bastante improbable que el proyecto evolucione mucho más.
+* Además, no tengo precisamente mucha idea del lenguaje LUA. Literalmente esta es la primera vez que tengo que tocarlo. No lo he disfrutado demasiado, la verdad sea dicha.
+* ZIM es una herramienta fantástica, uno de los programas más útils que he usado. Si no lo conoces, pruébalo. Eso sí, el esquema de colores por defecto es horrible, cámbialo en cuanto puedas.
+
+
+## Some notes
+
+* Bold and italic can only be used on words of the same line. That's more than enough for me, but feel free to improve it.
+* Comments in Spanish in the code and in the status bar messages. Should you have any issue, feel free to change it.  
+* This is just a personal project and the way it is right now is more than enough for my needs, so it's not likely to be devoloped much further any time soon.  
+* To top it all, I have no clue about the LUA programming language. This is the first time I had to use it. I didn't enjoy it a lot.  
+* ZIM is a superb tool, one of the most useful program I've ever used. If you don't use it yet: try it. But the default color scheme is awful, you'll need to change it.  
+
+
+# TODO
+* Help file inside micro
+* Bugs corrections
+* Send to Plugin Channel [https://github.com/micro-editor/plugin-channel]
--- microzim.lua
+++ microzim.lua
@@ -1,21 +1,24 @@
 VERSION = "0.0.1"
 
-
 local micro = import("micro")
 local config = import("micro/config")
 local buffer = import("micro/buffer")
+local util = import("micro/util")
 
---micro.InfoBar():Message("YEAH") --Mensajes en la barra de información
---buf:Insert( buffer.Loc(0,2), txtNuevo) --Inserta en la primera col de la línea 3
---micro.InfoBar():Message( "Lineas: ".. buf:LinesNum())
--- buf:MoveLinesDown(0,buf:LinesNum()-1) //rota las líneas
+function header()
+   local buf = micro.CurPane().Buf
+   local c = micro.CurPane().Cursor
+
+   buf:Insert( buffer.Loc(0, 0),  "Content-Type: text/x-zim-wiki\n")
+   buf:Insert( buffer.Loc(0, 1),  "Wiki-Format: zim 0.6\n")
+   --buf:Insert( buffer.Loc(0, 2),  "Creation-Date: 2020-02-06T09:31:36+01:00\n")
+end
+
 
 function verbatim()
 	local buf = micro.CurPane().Buf
-	local marca = "\'\'\'"
 	local c = micro.CurPane().Cursor
-	--local seleccion = c:GetSelection();
-
+	
 	if c:HasSelection() then
 		local inicio, fin = nil, nil
 		if c.CurSelection[1]:GreaterThan(-c.CurSelection[2]) then
@@ -25,20 +28,73 @@ function verbatim()
 			inicio = c.CurSelection[1]
 			fin = c.CurSelection[2]
 		end
-		
-		local a = buffer.Loc(0, inicio.Y)
-		local b = buffer.Loc(0, fin.Y)
 
-		micro.InfoBar():Message( "Líneas: " .. inicio.Y .. " - " ..  fin.Y)
+      -- Si tenemos más de una línea seleccionada o no
+      if inicio.Y == fin.Y then
+         local seleccion = c:GetSelection() --devuelve array de bytes
+         local texto = util.String(seleccion)
+         micro.InfoBar():Message( "Texto afectado: " .. texto )
+         
+         texto = "\'\'" .. texto .. "\'\'";
+         LocInicio = buffer.Loc(inicio.X, inicio.Y)
+         LocFin    = buffer.Loc(fin.X, fin.Y)
+         buf:Replace(LocInicio, LocFin, texto)
+      else
+   		local a = buffer.Loc(0, inicio.Y)
+   		local b = buffer.Loc(0, fin.Y)
+   		local marca = "\'\'\'"
 
-		buf:Insert( b, marca .. "\n")
-		buf:Insert( a, marca .. "\n")
+   		micro.InfoBar():Message( "Líneas afectadas: " .. inicio.Y .. " - " ..  fin.Y)
 
+   		buf:Insert( b, marca .. "\n")
+   		buf:Insert( a, marca .. "\n")
+   	end
 	else
 		micro.InfoBar():Message( "Ninguna línea seleccionada")
 	end
 end
 
+
+function italic()
+	italicOrBoldAuxiliar("//")
+end
+
+function bold()
+   italicOrBoldAuxiliar("**")
+end
+
+
+function italicOrBoldAuxiliar(marca)
+	local buf = micro.CurPane().Buf
+	local c = micro.CurPane().Cursor
+	
+	if c:HasSelection() then
+		local inicio, fin = nil, nil
+		if c.CurSelection[1]:GreaterThan(-c.CurSelection[2]) then
+			inicio = c.CurSelection[2]
+			fin = c.CurSelection[1]
+		else
+			inicio = c.CurSelection[1]
+			fin = c.CurSelection[2]
+		end
+
+      -- Si tenemos más de una línea seleccionada o no
+      if inicio.Y == fin.Y then
+         local seleccion = c:GetSelection() --devuelve array de bytes
+         local texto = util.String(seleccion)
+         micro.InfoBar():Message( "Texto afectado: " .. texto )
+         
+         texto = marca .. texto .. marca;
+         LocInicio = buffer.Loc(inicio.X, inicio.Y)
+         LocFin    = buffer.Loc(fin.X, fin.Y)
+         buf:Replace(LocInicio, LocFin, texto)
+      else
+   		micro.InfoBar():Message( "Error: Más de una línea seleccionada")
+   	end
+	else
+		micro.InfoBar():Message( "Error: Nada seleccionado")
+	end
+end
 
 
 function h1()
@@ -58,32 +114,43 @@ function h4()
 end
 
 function h5()
-	heading("==")
+   heading("==")
 end
 
 
 function heading(formato)
 	local buf = micro.CurPane().Buf
-    local numLinea = micro.CurPane().Cursor.Loc.Y --las líneas comienzan a contarse desde la 0
+   local numLinea = micro.CurPane().Cursor.Loc.Y --las líneas comienzan a contarse desde la 0
 	local eol = string.len(micro.CurPane().Buf:Line(micro.CurPane().Cursor.Loc.Y))
 
-    local txtViejo = buf:Line(numLinea)
-    local txtNuevo = formato .. " " .. txtViejo .. " " .. formato   
+   local txtViejo = buf:Line(numLinea)
+   local txtNuevo = formato .. " " .. txtViejo .. " " .. formato   
     
-    local LocInicioLinea = buffer.Loc(0,numLinea)
-    local LocFinLinea = buffer.Loc(eol,numLinea)
-    buf:Replace(LocInicioLinea, LocFinLinea, txtNuevo)
+   local LocInicioLinea = buffer.Loc(0,numLinea)
+   local LocFinLinea = buffer.Loc(eol,numLinea)
+   buf:Replace(LocInicioLinea, LocFinLinea, txtNuevo)
 end
 
 
 
 function init()
-    config.MakeCommand("h1", h1, config.NoComplete)
+   config.MakeCommand("h1", h1, config.NoComplete)
+   config.MakeCommand("h2", h2, config.NoComplete)
+   config.MakeCommand("h3", h3, config.NoComplete)
+   config.MakeCommand("h4", h4, config.NoComplete)
+   config.MakeCommand("h5", h5, config.NoComplete)
+   config.MakeCommand("verbatim", verbatim, config.NoComplete)
+   config.MakeCommand("header"  , header,   config.NoComplete)
+   config.MakeCommand("bold"    , bold,     config.NoComplete)
+   config.MakeCommand("italic"  , italic,   config.NoComplete)
 	config.TryBindKey("Alt-1", "lua:microzim.h1",false);
 	config.TryBindKey("Alt-2", "lua:microzim.h2",false);
 	config.TryBindKey("Alt-3", "lua:microzim.h3",false);
 	config.TryBindKey("Alt-4", "lua:microzim.h4",false);
 	config.TryBindKey("Alt-5", "lua:microzim.h5",false);
 	config.TryBindKey("Alt-t", "lua:microzim.verbatim",false);
-  --  config.AddRuntimeFile("manipulator", config.RTHelp, "help/manipulator.md")
+	config.TryBindKey("Alt-h", "lua:microzim.header",false);
+	config.TryBindKey("Alt-b", "lua:microzim.bold",false);
+	config.TryBindKey("Alt-i", "lua:microzim.italic",false);
+  --  config.AddRuntimeFile("microzim", config.RTHelp, "help/microzim.md")
 end
